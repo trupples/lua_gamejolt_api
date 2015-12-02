0cef388cfd1bb862b8671d1f32b1dcebbe523847
Dragomir Ioan <123ioandragomir123@gmail.com>
2015-12-02 09:18:31 -0800

    tests

---
+++ test.lua
@@ -0,0 +1,10 @@
+local gamejolt = require("gamejolt_api")
+require("test.print_r")
+
+gamejolt:init(111240, "c20a6414a1e31a6d7f7a329a37c626a6")
+
+gamejolt:users_auth("ioanD", "d08cf8")
+
+print_r(gamejolt:scores_guest_add("20 points", "20", "Gigel Vampiru'", "secret extra data"))
+
+print_r(gamejolt:scores_global_fetch())
